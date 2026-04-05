874fab21bcb613d30a7c380de45ed44dc8613736
arifaji29 <arifajif@gmail.com>
2026-04-05 22:20:30 +0700

    warna

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -17,7 +17,8 @@
       <h1 class="text-4xl md:text-5xl font-extrabold mb-4 flex flex-col md:flex-row items-center justify-center gap-2">
         <template v-if="!isEditMode">
           {{ localContent.hero_title_1 }}
-          <span class="font-logo tracking-widest text-transparent bg-clip-text bg-lanraro-r from-indigo-400 to-purple-500">{{ localContent.hero_title_2 }}</span>
+          <!-- TYPO DIPERBAIKI DI SINI: bg-gradient-to-r -->
+          <span class="font-logo tracking-widest text-transparent bg-clip-text bg-linear-to-r from-indigo-400 to-purple-500">{{ localContent.hero_title_2 }}</span>
         </template>
         <template v-else>
           <input v-model="localContent.hero_title_1" class="bg-dark/80 border border-indigo-500 rounded-lg px-4 py-2 text-center text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 w-full md:w-auto">
