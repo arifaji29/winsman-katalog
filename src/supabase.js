2ba739896778e87fc6a576faf9b618bb550000e5
arifaji29 <arifajif@gmail.com>
2026-04-05 16:08:19 +0700

    change supabase js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,7 @@
-// src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-// Dapatkan URL dan Key ini dari Dashboard Supabase Anda:
-// Settings -> API -> Project URL & Project API Keys (anon/public)
-const supabaseUrl = 'https://vqfdqwigexotdakpgdga.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZmRxd2lnZXhvdGRha3BnZGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyOTYxMzMsImV4cCI6MjA5MDg3MjEzM30.Sr1mp8bF0Cv077jXLQPgGiJ3JE3cN8FqzVbpT-mjLoQ'
+// Mengambil URL dan Key dari file .env.local
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
